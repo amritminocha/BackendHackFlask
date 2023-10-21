e0787855b18f6f0305584699d43341934b7cab9a
amritminocha <minochaamrit@gmail.com>
2023-10-21 15:41:22 -0400

    gpt api

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
-
+from services.openai.chatgpt import get_openai_response
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return 'Hello, Flask!'
+
+
+@app.route('/gpt')
+def getResponse():
+    return get_openai_response()
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
