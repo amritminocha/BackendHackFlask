2d4b90a10412c3c7b76f3e7e86fc9b5d4366aa67
maryamtello1 <maryamt@umich.edu>
2023-10-22 00:32:16 -0400

    Added multilingual support

--- Modules/tts.py
+++ Modules/tts.py
@@ -12,5 +12,42 @@ def get_voices():
     }
     response = requests.get(url, headers=headers)
     return response.json()["voices"]
+print()
 
+all_voices = get_voices()
 
+def generate_audio(text, output_path) -> str:
+    voices = all_voices
+
+    if not voices:
+        print("No voices found")
+        return
+    
+    try:
+        voice_id = next(filter(lambda v: v["name"] == voice_name, voices))["voice_id"]
+    except StopIteration:
+        voice_id = voices[0]["voice_id"]
+
+    url = f"https://api.elevenlabs.io/v1/text-to-speech/{voice_id}"
+    headers = {
+        "xi-api-key": ELEVENLABS_API_KEY,
+        "content-type": "application/json"
+    }
+    data = {
+        "text": text,
+        "voice_settings": {
+            "stability": stability,
+            "similarity_boost": similarity,
+        },
+        "model_id": "eleven_multilingual_v2"
+    }
+    response = requests.post(url, json=data, headers=headers)
+
+    # Generate a unique filename using uuid
+    unique_filename = f"{uuid.uuid4()}.mp3"
+    full_output_path = os.path.join(output_path, unique_filename)
+
+    with open(full_output_path, "wb") as output:
+        output.write(response.content)
+
+    return full_output_path
