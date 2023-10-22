659a183d622c6513adf576f3a0ef6b4a4090aa55
maryamtello1 <maryamt@umich.edu>
2023-10-22 01:48:28 -0400

    Added modules to flask app

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
 from services.openai.chatgpt import get_openai_response
+from Modules.elevenlabs import generate_audio
+from Modules.pptx_generator import 
 app = Flask(__name__)
 
 @app.route('/')
