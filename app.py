0ced72e1aa59e656b80054ff248cba4c3bc52036
maryamtello1 <maryamt@umich.edu>
2023-10-22 02:28:00 -0400

    imported save_pptx_as_png

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from services.openai.chatgpt import get_openai_response
 from Modules.pptx_generator import create_presentation
 from Modules.elevenlabs import generate_audio
+from Utilities.slide_to_image import save_pptx_as_png
 
 app = Flask(__name__)
 
@@ -15,6 +16,8 @@ def getResponse():
     topic = request.args.get('topic')
     total_slides = int(request.args.get('total_slides'))
     return get_openai_response(topic, total_slides)
+
+
 
 
 if __name__ == '__main__':
